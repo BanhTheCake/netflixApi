82794bb938049eb2331f8914e8d378d823cad7bd
Binhanh <binhanh080103@gmail.com>
2023-03-14 13:48:54 +0700

    config cookies

--- src/utils/configOptions/cookie.config.ts
+++ src/utils/configOptions/cookie.config.ts
@@ -2,7 +2,7 @@ import { CookieOptions } from 'express';
 
 export const cookieConfig: CookieOptions = {
   httpOnly: true,
-  secure: false,
-  sameSite: 'lax',
+  secure: true,
+  sameSite: 'strict',
   maxAge: 1000 * 60 * 60 * 24, // 1 days
 };
