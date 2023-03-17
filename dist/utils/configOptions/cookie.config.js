090243d5237a6a97bceb16988beb2c34a75a2398
Binhanh <binhanh080103@gmail.com>
2023-03-17 13:26:55 +0700

    fix cookieConfig 5

--- dist/utils/configOptions/cookie.config.js
+++ dist/utils/configOptions/cookie.config.js
@@ -6,7 +6,7 @@ exports.cookieConfig = {
     secure: true,
     sameSite: 'none',
     maxAge: 1000 * 60 * 60 * 24,
-    domain: '.vercel.app/',
+    domain: 'netflix-api-kappa.vercel.app',
     path: '/',
 };
 //# sourceMappingURL=cookie.config.js.map
